998d16f76c88163951d5a83f2f4d05792ef6001d
Ryan James <realcoder2023@gmail.com>
2025-02-25 16:40:38 -0500

    ad list of frameworks and added some list of programming languages if someone forgot

---
+++ Lists/Todos.js
@@ -0,0 +1,49 @@
+﻿import {
+    command,
+    cssFrameworkPicked,
+    getFrameworkTodo,
+    isEmpty,
+    listProgrammingLanguages,
+    programmingLanguages
+} from "../lists.js";
+
+import {question} from 'readline-sync';
+
+export const languages = [];
+
+
+const frameworkTodo = question("Enter Framework: ");
+const pickCSSLibrary = question("Enter Name of CSS Library: ")
+const language = question("Enter Programming Language for Project: ");
+
+
+
+languages.push("JavaScript");
+languages.push("Python");
+languages.push("C++");
+languages.push("C#");
+languages.push("C");
+languages.push("Java");
+
+if (language.startsWith("list") ) {
+    for (const a of languages) {
+        console.log(listProgrammingLanguages(a));
+
+    }
+}
+
+if (isEmpty(language)) {
+    command("Empty...");
+}
+if (isEmpty(pickCSSLibrary)) {
+    command("Empty...");
+}
+if (isEmpty(frameworkTodo)) {
+    command("Empty...");
+}
+else {
+    programmingLanguages(language);
+    cssFrameworkPicked(pickCSSLibrary);
+    getFrameworkTodo(frameworkTodo);
+
+}
---
+++ lists.js
@@ -0,0 +1,28 @@
+﻿export function getMessage(message) {
+    console.log(message);
+}
+
+export function command(command) {
+    getMessage(command);
+}
+
+export function getFrameworkTodo(framework) {
+    command("You Have Picked: " + framework)
+
+}
+
+export function isEmpty(a) {
+    return a === "";
+}
+
+export function cssFrameworkPicked(cssFrame) {
+    command("CSS Library is: " + cssFrame);
+}
+
+export function listProgrammingLanguages (list) {
+    return new Array(list);
+}
+
+export function programmingLanguages(lang) {
+    command("You Have Picked: " + lang + " In Your Project")
+}
