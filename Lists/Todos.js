160891c71a4be1b4efb7bf519ad152b0303530e4
Ryan James <realcoder2023@gmail.com>
2025-02-26 11:35:06 -0500

    Added Library Functions Questions

--- Lists/Todos.js
+++ Lists/Todos.js
@@ -1,49 +1,64 @@
 ﻿import {
-    command,
-    cssFrameworkPicked,
-    getFrameworkTodo,
-    isEmpty,
-    listProgrammingLanguages,
-    programmingLanguages
+  backendMobileFramework,
+  command,
+  cssFrameworkPicked,
+  frontendMobileFramework,
+  getFrameworkTodo,
+  isEmpty,
+  listProgrammingLanguages,
+  programmingLanguages,
 } from "../lists.js";
 
-import {question} from 'readline-sync';
+import { question } from "readline-sync";
 
 export const languages = [];
+export const Type = [];
 
+Type.push("Web");
+Type.push("Mobile");
+Type.push("System");
+Type.push("AI");
+Type.push("ML");
 
-const frameworkTodo = question("Enter Framework: ");
-const pickCSSLibrary = question("Enter Name of CSS Library: ")
-const language = question("Enter Programming Language for Project: ");
+const includes = question("Enter Type of Coding: ");
 
+if (includes.includes("Web")) {
+  const frameworkTodo = question("Enter Framework: ");
+  const pickCSSLibrary = question("Enter Name of CSS Library: ");
+  const language = question("Enter Programming Language for Project: ");
 
+  languages.push("JavaScript");
+  languages.push("Python");
+  languages.push("C++");
+  languages.push("C#");
+  languages.push("C");
+  languages.push("Java");
 
-languages.push("JavaScript");
-languages.push("Python");
-languages.push("C++");
-languages.push("C#");
-languages.push("C");
-languages.push("Java");
-
-if (language.startsWith("list") ) {
+  if (language.startsWith("list")) {
     for (const a of languages) {
-        console.log(listProgrammingLanguages(a));
-
+      console.log(listProgrammingLanguages(a));
     }
-}
+  }
 
-if (isEmpty(language)) {
+  if (isEmpty(language)) {
     command("Empty...");
-}
-if (isEmpty(pickCSSLibrary)) {
+  }
+  if (isEmpty(pickCSSLibrary)) {
     command("Empty...");
-}
-if (isEmpty(frameworkTodo)) {
+  }
+  if (isEmpty(frameworkTodo)) {
     command("Empty...");
-}
-else {
+  } else {
     programmingLanguages(language);
     cssFrameworkPicked(pickCSSLibrary);
     getFrameworkTodo(frameworkTodo);
+  }
+}
 
+if (includes.includes("Mobile")) {
+  const mobileFrontend = question("Enter Mobile Frontend Framework: ");
+  frontendMobileFramework(mobileFrontend);
+
+  const backendLibrary = question("Enter Backend Library: ");
+  backendMobileFramework(backendLibrary);
 }
